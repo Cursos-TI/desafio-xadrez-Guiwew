8503ef60e82864ff176bc00eddfe57f8d0be9030
Guilherme Marques de Moura <guiwew2004@hotmail.com>
2025-08-18 23:02:13 +0000

    aventureiro

--- xadrez.c
+++ xadrez.c
@@ -1,30 +1,37 @@
 #include <stdio.h>
 
 int main() {
-    int bispo =1,torre=1, rainha=1;//adicionar as peças
+    int bispo =1,torre=1, rainha=1, cavalo;//adicionar as peças
 
         printf("***movimento do bispo***\n");
-    while (bispo<=5)//inicio do while e movimento do bispo a 5 casas diagonal direita
-    {
+    while (bispo<=5){//inicio do while e movimento do bispo a 5 casas diagonal direita
         printf("cima direita\n");
         bispo++;  }//fim do while
-    
 
         printf("***movimento da torre***\n");
-    do//inicio do do-while e movimento da torre 5 casas a direita
-    {
+    do{//inicio do do-while e movimento da torre 5 casas a direita
         printf("direita\n");
         torre++;
     } while (torre<=5);//fim do do-while
 
     printf("***movimento da rainha***\n");
-    for (rainha = 1; rainha<=8; rainha++)//inicio do for-while e movimento da rainha 8 casas a esquerda
-    {
+    for (rainha = 1; rainha<=8; rainha++){//inicio do for-while e movimento da rainha 8 casas a esquerda
         printf("esquerda\n");  }//fim do for-while
     
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    printf("***movimento do cavalo***\n");
+    for (cavalo=1; cavalo<=1; cavalo++){//inicio do for-while e movimento do cavalo 2 casas baixo e 1 esquerda
+        int cavalodiagonal=1;
+
+        while (cavalodiagonal<=2){//começo do while interno
+            printf("baixo, ");
+            cavalodiagonal++;
+        }//fim do while interno
+
+        printf("esquerda\n");
+        
+    }//fim do for-while
+    
+
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
